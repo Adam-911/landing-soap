5ceb5bd019012aac1a9b50fed2a82c4176fb7787
Ivan Savitskiy <ivan.savitskiy@asia-soft.kz>
2021-04-28 16:57:40 +0600

    Доделал вызов модальных форм при клике по карточке

--- src/js/script.js
+++ src/js/script.js
@@ -1,49 +1,46 @@
-const modalTrigger = document.querySelectorAll('[data-modal]'),
-        modal = document.querySelector('.modal');
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+        modal = document.querySelectorAll('.modal');
+        console.log("modal");
+        console.log(modal);
+        console.log("modalTrigger");
+        console.log(modalTrigger);
+    let activeOverlay = undefined;
 
     modalTrigger.forEach(btn => {
         btn.addEventListener('click', openModal);
     });
 
     function closeModal() {
-        modal.classList.add('hide');
-        modal.classList.remove('show');
+        const el = document.getElementById(activeOverlay.id);
+        el.classList.add('hide');
+        el.classList.remove('show');
         document.body.style.overflow = '';
     }
 
-    function openModal() {
-        modal.classList.add('show');
-        modal.classList.remove('hide');
+    function openModal(btn) {
+        // const id = Number(btn.target.id) + 100;
+        const id = Number(btn.target.id) + 100;
+        activeOverlay = document.getElementById(id);
+        activeOverlay.classList.add('show');
+        activeOverlay.classList.remove('hide');
         document.body.style.overflow = 'hidden';
-        clearInterval(modalTimerId);
     }
 
-    modal.addEventListener('click', (e) => {
-        if (e.target === modal || e.target.getAttribute('data-close') == "") {
-            closeModal();
-        }
+    modal.forEach(mod => {
+        mod.addEventListener('click', (e) => {
+            console.log("el");
+            console.log(e);
+            if (e.target === modal || e.target.getAttribute('data-close') == "") {
+                closeModal();
+            }
+        });
     });
-
-    document.addEventListener('keydown', (e) => {
-        if (e.code === "Escape" && modal.classList.contains('show')) { 
-            closeModal();
-        }
-    });
-
-    const modalTimerId = setTimeout(openModal, 300000);
-    // Изменил значение, чтобы не отвлекало
-
-    function showModalByScroll() {
-        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
-            openModal();
-            window.removeEventListener('scroll', showModalByScroll);
-        }
-    }
-    window.addEventListener('scroll', showModalByScroll);
 
     class Product {
-        constructor(src, alt, title, descr, price, parentSelector, ...classes) {
-            this.src = src;
+        constructor(childId, img, subSrc, alt, title, descr, price, parentSelector, ...classes) {
+            this.childId = childId;
+            this.img = img;
+            this.subSrc = subSrc;
             this.alt = alt;
             this.title = title;
             this.descr = descr;
@@ -55,22 +52,37 @@ const modalTrigger = document.querySelectorAll('[data-modal]'),
 
         render() {
             const element = document.createElement('div');
+            element.id = this.childId;
 
             if (this.classes.length === 0) {
-                this.classes = "menu__item";
+                this.classes = "modal";
                 element.classList.add(this.classes);
-            } else {
-                this.classes.forEach(className => element.classList.add(className));
             }
 
             element.innerHTML = `
-                <img src=${this.src} alt=${this.alt}>
-                <h3 class="menu__item-subtitle">${this.title}</h3>
-                <div class="menu__item-descr">${this.descr}</div>
-                <div class="menu__item-divider"></div>
-                <div class="menu__item-price">
-                    <div class="menu__item-cost">Цена:</div>
-                    <div class="menu__item-total"><span>${this.price}</span> т</div>
+                <div class="modal__dialog">
+                    <div class="modal__content">
+                        <form action="#">
+                            <div data-close class="modal__close" onClick="closeModal()">&times;</div>
+                            <div class="modal__preview">
+                                <img src=${this.img} alt="">
+                                <div class="modal__preview_title">${this.title}</div>
+                                <div class="modal__preview_descr">${this.descr}</div>
+                                <div class="modal__preview_text">${this.price}</div>
+                            </div>
+                            <div class="modal__order">
+                                <div class="modal__order_title">Заказать можно здесь:</div>
+                                <a href="#">
+                                    <img src="icons/inst_modal.svg" alt="" class="modal__order_img">
+                                </a>
+                                <div class="modal__order_subtitle">или
+                                    оставить заявку:</div>
+                                <input required placeholder="Ваше имя" name="name" type="text" class="modal__order_input">
+                                <input required placeholder="Ваш номер телефона" name="phone" type="phone" class="modal__order_input">
+                                <button class="modal__order_btn">Отправить</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             `;
             this.parent.append(element);
@@ -78,19 +90,24 @@ const modalTrigger = document.querySelectorAll('[data-modal]'),
     }
 
     const getResource = async (url) => {
-        const res = await fetch(url);
+    const res = await fetch(url);
 
-        if (!res.ok) {
-            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
-        }
-
+    if (!res.ok) {
+        throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    }
         return await res.json();
     };
 
     getResource('http://localhost/db.json')
         .then(data => {
             data = data.product
-            data.forEach(({img, altimg, title, descr, price}) => {
-                new Product(img, altimg, title, descr, price, '.menu .container').render();
+            data.forEach(({childId, img, subSrc, altimg, title, descr, price}) => {
+                new Product(childId, img, subSrc, altimg, title, descr, price, '.catalog-test').render();
             });
-        });
+    });
+    const modal2 = document.querySelectorAll('.modal');
+    console.log("document");
+    console.log(document);
+    console.log("modal2");
+    console.log(document.querySelectorAll('modal'));
+
