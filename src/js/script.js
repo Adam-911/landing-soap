ae7967babc35a1fea56f94ad10f0fddd3e3d2e02
Adam <21wsqx@dwd.ey>
2021-05-18 22:12:42 +0600

    Добавил запрос на сервер в модальном окне

--- src/js/script.js
+++ src/js/script.js
@@ -72,12 +72,10 @@
                                 </a>
                                 <div class="modal__order_subtitle">или
                                     оставить заявку:</div>
-                                <form onsubmit="event.preventDefault();onSubmit('modal');return false;">
-                                    <input required placeholder="Ваше имя" id="modal_name" name="name" type="text" class="modal__order_input">
-                                    <input required placeholder="Ваш номер телефона или почта" id="modal_contact" name="phone" type="phone" class="modal__order_input">
-                                    <button class="modal__order_btn">Отправить</button>
-                                </form>
-                            </div>
+                                <input required placeholder="Ваше имя" id="modal_name" name="name" type="text" class="modal__order_input">
+                                <input required placeholder="Ваш номер телефона или почта" id="modal_contact" name="phone" type="phone" class="modal__order_input">
+                                <button type="button" onclick="onSubmit('modal')" class="modal__order_btn">Отправить</button>
+                            </div>1
                         </form>
                     </div>
                 </div>
